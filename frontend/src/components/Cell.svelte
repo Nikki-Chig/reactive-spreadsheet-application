20e3a30e0100f07f03974fddb1f2010e5f1e7d67
Nikki-Chig <nikitachigateri@gmail.com>
2025-01-18 20:14:53 -0500

    Fixed the double clicking cell bug

--- frontend/src/components/Cell.svelte
+++ frontend/src/components/Cell.svelte
@@ -1,52 +1,61 @@
-<!-- frontend/src/components/Cell.svelte -->
 <script>
-    import { createEventDispatcher } from 'svelte';
-    export let row;
-    export let col;
-    export let value = '';
-    const dispatch = createEventDispatcher();
-    let editing = false;
-    let inputValue = value;
-  
-    function handleDoubleClick() {
-      editing = true;
-    }
-  
-    function handleBlur() {
-      editing = false;
+  import { createEventDispatcher } from 'svelte';
+  export let row;
+  export let col;
+  export let value = '';
+  const dispatch = createEventDispatcher();
+  let editing = false;
+  let inputValue;
+
+  // Synchronize inputValue with value when not editing
+  $: if (!editing) {
+    inputValue = value;
+  }
+
+  function handleDoubleClick() {
+    editing = true;
+    // Ensure inputValue is synced with value when editing starts
+    inputValue = value;
+  }
+
+  function handleBlur() {
+    editing = false;
+    if (inputValue !== value) {
       dispatch('update', inputValue);
     }
-  
-    function handleKeyDown(event) {
-      if (event.key === 'Enter') {
-        editing = false;
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      editing = false;
+      if (inputValue !== value) {
         dispatch('update', inputValue);
       }
     }
-  </script>
-  
-  <div on:dblclick={handleDoubleClick}>
-    {#if editing}
-      <input
-        bind:value={inputValue}
-        on:blur={handleBlur}
-        on:keydown={handleKeyDown}
-        autofocus
-      />
-    {:else}
-      {value}
-    {/if}
-  </div>
-  
-  <style>
-    div {
-      min-width: 100px;
-      min-height: 30px;
-    }
-    input {
-      width: 100%;
-      height: 100%;
-      box-sizing: border-box;
-    }
-  </style>
-  
+  }
+</script>
+
+<div on:dblclick={handleDoubleClick}>
+  {#if editing}
+    <input
+      bind:value={inputValue}
+      on:blur={handleBlur}
+      on:keydown={handleKeyDown}
+      autofocus
+    />
+  {:else}
+    {value}
+  {/if}
+</div>
+
+<style>
+  div {
+    min-width: 100px;
+    min-height: 30px;
+  }
+  input {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+  }
+</style>
